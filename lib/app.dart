ee66d5b0df6902ceb70cd00a7063453bd9c838cf
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-01-01 14:11:31 +0600

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:craft_bay/presentation/ui/screens/splash_screen.dart';
+import 'package:craft_bay/presentation/ui/utility/app_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class CratfyBay extends StatelessWidget {
@@ -7,6 +8,7 @@ class CratfyBay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  GetMaterialApp(
+      theme: AppThemeData.lightThemeData,
       debugShowCheckedModeBanner: false,
       home:Splash_Screen(),
     );
--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:craft_bay/presentation/ui/screens/verity_email_screen.dart';
+import 'package:craft_bay/presentation/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({super.key});
@@ -8,10 +11,34 @@ class Splash_Screen extends StatefulWidget {
 }
 
 class _Splash_ScreenState extends State<Splash_Screen> {
+
+  @override
+  void initState() {
+    super.initState();
+    moveToNextScreen();
+  }
+
+
+  void moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2),);
+    Get.offAll(const VerifyEmailScreen());
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(),
+            AppLogo(),
+            Spacer(),
+            CircularProgressIndicator(),
+            SizedBox(height: 16,),
+            Text('Version 1.0'),
+            SizedBox(height: 16,),
+          ],
+        ),
+      ),
     );
   }
 }
