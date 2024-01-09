0e1be656ded1f2c3f68d0550ab2f4fd8ab59f409
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-01-09 19:11:38 +0600

    commit

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:craft_bay/presentation/ui/utility/assets_path.dart';
 import 'package:craft_bay/presentation/ui/widgets/home/banner_carousel.dart';
 import 'package:craft_bay/presentation/ui/widgets/home/section_title.dart';
 import 'package:flutter/material.dart';
+import '../utility/app_colors.dart';
 import '../widgets/category_item.dart';
 import '../widgets/home/circle_icon_botton.dart';
 
@@ -26,9 +27,9 @@ class HomeScreen extends StatelessWidget {
               SectionTitle(title: "All Category", onTapSeeall: (){
               }),
               category_list,
-              SectionTitle(title: "popular", onTapSeeall: (){
-
+              SectionTitle(title: "Popular", onTapSeeall: (){
               }),
+              popular_list,
 
 
 
@@ -56,6 +57,101 @@ class HomeScreen extends StatelessWidget {
                 },
               ),
             );
+  }
+
+  SizedBox get popular_list {
+    return SizedBox(
+      height: 190,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        primary: false,
+        itemCount: 10,
+        itemBuilder: ( context, index) {
+          return SizedBox(
+            height: 190,
+            width: 160,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16)
+                    ),
+                    child: Image.asset(AssetsPath.dammyshoe,
+                      width:160,
+                      height: 120,
+                      fit:BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("nike show 120h new collection",
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                            overflow: TextOverflow.ellipsis),
+                        ),
+                        Row(
+                          children: [
+                            Text("\$120",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w600),
+                        ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              children: [
+                                Icon(Icons.star,size: 14,color: Colors.amber,),
+                                Text("4.4",style:
+                                  TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black45),
+                                  ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Card(
+                              color: AppColors.primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: Icon(Icons.favorite_border_outlined,size: 12,color: Colors.white,),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: ( _, __ ) {
+          return SizedBox(width: 8,);
+        },
+      ),
+    );
   }
 
   TextFormField get search_text_feild {
