6354e87874097a2e5ca8bc520d29db56586939c5
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-01-09 21:16:56 +0600

    commit

--- lib/presentation/ui/screens/category_screen.dart
+++ lib/presentation/ui/screens/category_screen.dart
@@ -1,33 +1,43 @@
+import 'package:craft_bay/presentation/state_holders/main_bottom_nav_controller.dart';
 import 'package:craft_bay/presentation/ui/widgets/category_item.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("Category",style: TextStyle(fontSize: 18),),
-        leading: IconButton(onPressed: (){},
-            icon: Icon(Icons.arrow_back_ios)),
-        elevation: 3,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: GridView.builder(
-          itemCount: 20,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              childAspectRatio: 0.95,
-              mainAxisSpacing: 12,
-              crossAxisSpacing: 8,
-            ),
-            itemBuilder: (context,index){
-              return FittedBox(
-                  child: CategoryItem());
-            }),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (value){
+        Get.find<MainBottomNabController>().BackToHome();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text("Category",style: TextStyle(fontSize: 18),),
+          leading: IconButton(onPressed: (){
+            Get.find<MainBottomNabController>().BackToHome();
+          },
+              icon: Icon(Icons.arrow_back_ios)),
+          elevation: 3,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: GridView.builder(
+            itemCount: 20,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                childAspectRatio: 0.95,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 8,
+              ),
+              itemBuilder: (context,index){
+                return FittedBox(
+                    child: CategoryItem());
+              }),
+        ),
       ),
     );
   }
--- lib/presentation/ui/screens/wishList.dart
+++ lib/presentation/ui/screens/wishList.dart
@@ -1,10 +1,46 @@
+import 'package:craft_bay/presentation/ui/widgets/product_card_item.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../state_holders/main_bottom_nav_controller.dart';
 
 class WishListScreen extends StatelessWidget {
   const WishListScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (value){
+        Get.find<MainBottomNabController>().BackToHome();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text("Wishlist",style: TextStyle(fontSize: 18),),
+          leading: IconButton(onPressed: (){
+            Get.find<MainBottomNabController>().BackToHome();
+          },
+              icon: Icon(Icons.arrow_back_ios)),
+          elevation: 3,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: GridView.builder(
+              itemCount: 20,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 0.95,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 4,
+              ),
+              itemBuilder: (context,index){
+                return FittedBox(
+                    child: ProductcCardItem());
+              }),
+        ),
+      ),
+    );
   }
 }
