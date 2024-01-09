3bd3eb10e8435e2738269377729706fb9e238616
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-01-09 19:17:12 +0600

    commit

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import '../utility/app_colors.dart';
 import '../widgets/category_item.dart';
 import '../widgets/home/circle_icon_botton.dart';
+import '../widgets/product_card_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -30,9 +31,14 @@ class HomeScreen extends StatelessWidget {
               SectionTitle(title: "Popular", onTapSeeall: (){
               }),
               popular_list,
-
-
-
+              SizedBox(height: 8,),
+              SectionTitle(title: "Special", onTapSeeall: (){
+              }),
+              popular_list,
+              SizedBox(height: 8,),
+              SectionTitle(title: "New", onTapSeeall: (){
+              }),
+              popular_list,
 
             ],
           ),
@@ -68,84 +74,7 @@ class HomeScreen extends StatelessWidget {
         primary: false,
         itemCount: 10,
         itemBuilder: ( context, index) {
-          return SizedBox(
-            height: 190,
-            width: 160,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16)
-                    ),
-                    child: Image.asset(AssetsPath.dammyshoe,
-                      width:160,
-                      height: 120,
-                      fit:BoxFit.cover,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text("nike show 120h new collection",
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black54,
-                            overflow: TextOverflow.ellipsis),
-                        ),
-                        Row(
-                          children: [
-                            Text("\$120",
-                          style: TextStyle(
-                              fontSize: 12,
-                              color: AppColors.primaryColor,
-                              fontWeight: FontWeight.w600),
-                        ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            Wrap(
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              children: [
-                                Icon(Icons.star,size: 14,color: Colors.amber,),
-                                Text("4.4",style:
-                                  TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.black45),
-                                  ),
-                              ],
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            Card(
-                              color: AppColors.primaryColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(14),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: Icon(Icons.favorite_border_outlined,size: 12,color: Colors.white,),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
+          return ProductcCardItem();
         },
         separatorBuilder: ( _, __ ) {
           return SizedBox(width: 8,);
@@ -193,3 +122,5 @@ class HomeScreen extends StatelessWidget {
   }
 
 }
+
+
